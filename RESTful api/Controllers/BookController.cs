93304a2a1801c821eb3869e7ab50fdb41f9d6652
roo20 <kotaiba.jarkas@gmail.com>
2023-02-08 20:50:09 +0100

    adding Put and GetById endpoints

--- RESTful api/Controllers/BookController.cs
+++ RESTful api/Controllers/BookController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using RESTful_api.Data;
 using RESTful_api.Dtos;
+using RESTful_api.Models;
 
 namespace RESTful_api.Controllers;
 
@@ -21,10 +22,34 @@ public class BookController : ControllerBase
 	[HttpGet]
 	public ActionResult<IEnumerable<BookReadDto>> GetBooks()
 	{
-		Console.WriteLine("getting books...");
 		var bookItem= _bookRepo.GetAllBooks();
 		return Ok(_mapper.Map<IEnumerable<BookReadDto>>(bookItem));
 
 	}
+
+    [HttpGet("{id}",Name = "GetBookById")]
+    public ActionResult<BookReadDto> GetBookById(int id)
+	{
+        var bookItem = _bookRepo.GetBookById(id);
+        
+		if (bookItem != null)
+		{
+            return Ok(_mapper.Map<BookReadDto>(bookItem));
+
+        }
+		return NotFound();
+    }
+
+    [HttpPost]
+    public ActionResult<BookReadDto> CreateBook(BookCreateDto bookCreateDto)
+    {
+        var bookModel=_mapper.Map<Book>(bookCreateDto);
+        _bookRepo.CreateBook(bookModel);
+        _bookRepo.SaveChanges();
+
+        var bookReadDto=_mapper.Map<BookReadDto>(bookModel);
+
+        return CreatedAtRoute(nameof(GetBookById), new { id=bookReadDto.Id},bookReadDto );
+    }
 
 }
